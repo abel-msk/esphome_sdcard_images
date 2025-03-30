9622639c437ec9031b1fba4c7044e10e06745d75
abel-msk <Artem.Belyaev@gmail.com>
2025-03-30 20:55:08 +0300

    start

---
+++ components/sd_mmc_card/local_image/local_image.cpp
@@ -0,0 +1,407 @@
+#include "local_image.h"
+
+#include "esphome/core/log.h"
+
+static const char *const TAG = "local_image";
+
+#include "image_decoder.h"
+
+#ifdef USE_ONLINE_IMAGE_BMP_SUPPORT
+#include "bmp_image.h"
+#endif
+#ifdef USE_ONLINE_IMAGE_JPEG_SUPPORT
+#include "jpeg_image.h"
+#endif
+#ifdef USE_ONLINE_IMAGE_PNG_SUPPORT
+#include "png_image.h"
+#endif
+
+namespace esphome
+{
+    namespace local_image
+    {
+
+        using image::ImageType;
+
+        inline bool is_color_on(const Color &color)
+        {
+            // This produces the most accurate monochrome conversion, but is slightly slower.
+            //  return (0.2125 * color.r + 0.7154 * color.g + 0.0721 * color.b) > 127;
+
+            // Approximation using fast integer computations; produces acceptable results
+            // Equivalent to 0.25 * R + 0.5 * G + 0.25 * B
+            return ((color.r >> 2) + (color.g >> 1) + (color.b >> 2)) & 0x80;
+        }
+
+        LocalImage::LocalImage(sd_mmc_card::SdMmc *card, const std::string &path, int width, int height, ImageFormat format, ImageType type,
+                               image::Transparency transparency, uint32_t download_buffer_size)
+            : Image(nullptr, 0, 0, type, transparency),
+              buffer_(nullptr),
+              download_buffer_(download_buffer_size),
+              download_buffer_initial_size_(download_buffer_size),
+              format_(format),
+              fixed_width_(width),
+              fixed_height_(height)
+        {
+            this->set_path(path);
+            this->set_sd_mmc_card(card);
+            this->source_size_ = 0;
+            this->source_buffer_ = {nullptr};
+            this->loded = false;
+        }
+
+        void LocalImage::set_sd_mmc_card(sd_mmc_card::SdMmc *card) { 
+            this->sd_mmc_card_ = card; 
+        }
+
+        void LocalImage::draw(int x, int y, display::Display *display, Color color_on, Color color_off)
+        {
+            ESP_LOGD(TAG, "Draw image.");
+            
+            if (this->data_start_)
+            {
+                Image::draw(x, y, display, color_on, color_off);
+            }
+            else if (this->placeholder_)
+            {
+                this->placeholder_->draw(x, y, display, color_on, color_off);
+            }
+        }
+
+        void LocalImage::release()
+        {
+            if (this->buffer_)
+            {
+                ESP_LOGV(TAG, "Deallocating old buffer...");
+                this->allocator_.deallocate(this->buffer_, this->get_buffer_size_());
+                this->data_start_ = nullptr;
+                this->buffer_ = nullptr;
+                this->width_ = 0;
+                this->height_ = 0;
+                this->buffer_width_ = 0;
+                this->buffer_height_ = 0;
+                // this->end_connection_();
+
+                if (this->source_size_ > 0 ) {
+                    if (this->source_buffer_)
+                        this->allocator_.deallocate(this->source_buffer_,this->source_size_);
+                    this->source_size_ = 0;
+                }
+            
+            }
+        }
+
+        size_t LocalImage::resize_(int width_in, int height_in)
+        {
+            int width = this->fixed_width_;
+            int height = this->fixed_height_;
+            if (this->is_auto_resize_())
+            {
+                width = width_in;
+                height = height_in;
+                if (this->width_ != width && this->height_ != height)
+                {
+                    this->release();
+                }
+            }
+            size_t new_size = this->get_buffer_size_(width, height);
+            if (this->buffer_)
+            {
+                // Buffer already allocated => no need to resize
+                return new_size;
+            }
+            ESP_LOGD(TAG, "Allocating new buffer of %zu bytes", new_size);
+            this->buffer_ = this->allocator_.allocate(new_size);
+            if (this->buffer_ == nullptr)
+            {
+                ESP_LOGE(TAG, "allocation of %zu bytes failed. Biggest block in heap: %zu Bytes", new_size,
+                         this->allocator_.get_max_free_block_size());
+                // this->end_connection_();
+                return 0;
+            }
+            this->buffer_width_ = width;
+            this->buffer_height_ = height;
+            this->width_ = width;
+            ESP_LOGV(TAG, "New size: (%d, %d)", width, height);
+            return new_size;
+        }
+
+        /**********************************************************************************************
+         * 
+         * @brief Prepare for load image. Check is file exist, check image format and prepare encoder
+         * 
+         */
+        void LocalImage::update()
+        {
+            
+            ESP_LOGD(TAG, "Loading image from file : %s", this->path_.c_str());
+
+            size_t file_size = this->sd_mmc_card_->file_size(this->path_.c_str());
+            if (file_size <= 0) {
+                ESP_LOGE(TAG, "File not found: *s",path_.c_str());
+                return;
+            }
+
+            /*   Free mem form old source  */
+            if (this->source_size_ > 0) {
+                this->allocator_.deallocate(this->source_buffer_,this->source_size_);
+                this->source_size_ = 0;
+            }
+
+            /*    Get Memory for read file */
+            this->source_buffer_ =  this->allocator_.allocate(file_size);
+            if (this->source_buffer_ == nullptr)
+            {
+                ESP_LOGE(TAG, "No memory. (Size: %zu)", file_size);
+                return;
+            }
+            this->source_size_ = file_size;
+
+
+            /*    Read file */
+            ESP_LOGD(TAG, "Start read file: %s. Size=%d",path_.c_str(), file_size);
+                
+            size_t read_bytes = this->sd_mmc_card_->read_file(path_.c_str(),
+                                                        this->source_buffer_,file_size);
+            ESP_LOGD(TAG, "Read %d bytes", read_bytes);
+            if  (read_bytes <  file_size) {
+                ESP_LOGW(TAG, "Expct %d, read %d.", file_size, read_bytes);
+            }
+
+            /*   Clear old decoder */
+            if (this->decoder_)
+            {
+                this->decoder_.reset();
+                // delete *this->decoder_;
+                ESP_LOGD(TAG, "Reset decoder.");
+                this->decoder_ = nullptr;
+            }
+       
+            if (!this->decoder_)
+            {
+                ESP_LOGD(TAG, "Load decoder.");
+
+#ifdef USE_ONLINE_IMAGE_BMP_SUPPORT
+                if (this->format_ == ImageFormat::BMP)
+                {
+                    ESP_LOGD(TAG, "Allocating BMP decoder");
+                    this->decoder_ = make_unique<BmpDecoder>(this);
+                }
+#endif // ONLINE_IMAGE_BMP_SUPPORT
+#ifdef USE_ONLINE_IMAGE_JPEG_SUPPORT
+                if (this->format_ == ImageFormat::JPEG)
+                {
+                    ESP_LOGD(TAG, "Allocating JPEG decoder");
+                    this->decoder_ = esphome::make_unique<JpegDecoder>(this);
+                }
+#endif // USE_ONLINE_IMAGE_JPEG_SUPPORT
+#ifdef USE_ONLINE_IMAGE_PNG_SUPPORT
+                if (this->format_ == ImageFormat::PNG)
+                {
+                    ESP_LOGD(TAG, "Allocating PNG decoder");
+                    this->decoder_ = make_unique<PngDecoder>(this);
+                }
+#endif // ONLINE_IMAGE_PNG_SUPPORT
+            }
+
+            if (!this->decoder_)
+            {
+                ESP_LOGE(TAG, "Could not instantiate decoder. Image format unsupported: %d", this->format_);
+                // this->end_connection_();
+                // this->download_error_callback_.call();
+                return;
+            }
+            auto prepare_result = this->decoder_->prepare(read_bytes);
+            if (prepare_result < 0)
+            {
+                this->decoder_.reset();
+                return;
+            }
+
+            auto fed = this->decoder_->decode(this->source_buffer_, this->source_size_);
+            if (fed < 0)
+            {
+                ESP_LOGE(TAG, "Error when decoding image.");
+                this->decoder_.reset();
+                return;
+            }
+
+            if (this->decoder_->is_finished())
+            {
+                ESP_LOGD(TAG, "Decoder is finished");
+
+                // this->width_, this->height_);
+                this->data_start_ = buffer_;
+                this->width_ = buffer_width_;
+                this->height_ = buffer_height_;
+
+                ESP_LOGD(TAG, "Image fully loaded, read %zu bytes, width/height = %d/%d", fed, this->width_,this->height_);        
+                }
+            else {
+                ESP_LOGE(TAG, "Decoding is not complete.");
+                this->decoder_.reset();
+            }
+
+            this->loded = true;
+        }
+        /**********************************************************************************************
+         * 
+         * @brief Do load and decode image.
+         * 
+         */
+        void LocalImage::loop()
+        {
+
+            if (this->loded) {
+                this->load_finished_callback_.call();
+                this->loded = false;
+            }
+            return;
+        }
+
+        /**
+         * @brief 
+         * 
+         * @param color 
+         */
+        void LocalImage::map_chroma_key(Color &color)
+        {
+            if (this->transparency_ == image::TRANSPARENCY_CHROMA_KEY)
+            {
+                if (color.g == 1 && color.r == 0 && color.b == 0)
+                {
+                    color.g = 0;
+                }
+                if (color.w < 0x80)
+                {
+                    color.r = 0;
+                    color.g = this->type_ == ImageType::IMAGE_TYPE_RGB565 ? 4 : 1;
+                    color.b = 0;
+                }
+            }
+        }
+
+        void LocalImage::draw_pixel_(int x, int y, Color color)
+        {
+            if (!this->buffer_)
+            {
+                ESP_LOGE(TAG, "Buffer not allocated!");
+                return;
+            }
+            if (x < 0 || y < 0 || x >= this->buffer_width_ || y >= this->buffer_height_)
+            {
+                ESP_LOGE(TAG, "Tried to paint a pixel (%d,%d) outside the image!", x, y);
+                return;
+            }
+            uint32_t pos = this->get_position_(x, y);
+            switch (this->type_)
+            {
+            case ImageType::IMAGE_TYPE_BINARY:
+            {
+                const uint32_t width_8 = ((this->width_ + 7u) / 8u) * 8u;
+                pos = x + y * width_8;
+                auto bitno = 0x80 >> (pos % 8u);
+                pos /= 8u;
+                auto on = is_color_on(color);
+                if (this->has_transparency() && color.w < 0x80)
+                    on = false;
+                if (on)
+                {
+                    this->buffer_[pos] |= bitno;
+                }
+                else
+                {
+                    this->buffer_[pos] &= ~bitno;
+                }
+                break;
+            }
+            case ImageType::IMAGE_TYPE_GRAYSCALE:
+            {
+                uint8_t gray = static_cast<uint8_t>(0.2125 * color.r + 0.7154 * color.g + 0.0721 * color.b);
+                if (this->transparency_ == image::TRANSPARENCY_CHROMA_KEY)
+                {
+                    if (gray == 1)
+                    {
+                        gray = 0;
+                    }
+                    if (color.w < 0x80)
+                    {
+                        gray = 1;
+                    }
+                }
+                else if (this->transparency_ == image::TRANSPARENCY_ALPHA_CHANNEL)
+                {
+                    if (color.w != 0xFF)
+                        gray = color.w;
+                }
+                this->buffer_[pos] = gray;
+                break;
+            }
+            case ImageType::IMAGE_TYPE_RGB565:
+            {
+                this->map_chroma_key(color);
+                uint16_t col565 = display::ColorUtil::color_to_565(color);
+                this->buffer_[pos + 0] = static_cast<uint8_t>((col565 >> 8) & 0xFF);
+                this->buffer_[pos + 1] = static_cast<uint8_t>(col565 & 0xFF);
+                if (this->transparency_ == image::TRANSPARENCY_ALPHA_CHANNEL)
+                {
+                    this->buffer_[pos + 2] = color.w;
+                }
+                break;
+            }
+            case ImageType::IMAGE_TYPE_RGB:
+            {
+                this->map_chroma_key(color);
+                this->buffer_[pos + 0] = color.r;
+                this->buffer_[pos + 1] = color.g;
+                this->buffer_[pos + 2] = color.b;
+                if (this->transparency_ == image::TRANSPARENCY_ALPHA_CHANNEL)
+                {
+                    this->buffer_[pos + 3] = color.w;
+                }
+                break;
+            }
+            }
+        }
+
+        /*
+        void LocalImage::end_connection_()
+        {
+            // if (this->downloader_)
+            // {
+            //     this->downloader_->end();
+            //     this->downloader_ = nullptr;
+            // }
+
+            // if (this->source_size_ > 0 ) {
+            //     if (this->source_buffer_)
+            //         this->allocator_.deallocate(this->source_buffer_,this->source_size_);
+            //     this->source_size_ = 0;
+            // }
+            this->decoder_.reset();
+            this->download_buffer_.reset();
+        }
+        */
+        bool LocalImage::validate_path_(const std::string &path)
+        {
+            // TODO: Add path validation
+
+            // if ((url.length() < 8) || (url.find("http") != 0) || (url.find("://") == std::string::npos)) {
+            //   ESP_LOGE(TAG, "URL is invalid and/or must be prefixed with 'http://' or 'https://'");
+            //   return false;
+            // }
+            return true;
+        }
+
+        void LocalImage::add_on_finished_callback(std::function<void()> &&callback)
+        {
+            this->load_finished_callback_.add(std::move(callback));
+        }
+
+        void LocalImage::add_on_error_callback(std::function<void()> &&callback)
+        {
+            this->download_error_callback_.add(std::move(callback));
+        }
+
+    } // namespace online_image
+} // namespace esphome
---
+++ components/sd_mmc_card/local_image/local_image.h
@@ -0,0 +1,272 @@
+#pragma once
+
+// #include "esphome/components/http_request/http_request.h"
+// #define USE_ONLINE_IMAGE_JPEG_SUPPORT
+// #define USE_ONLINE_IMAGE_BMP_SUPPORT
+// #define USE_ONLINE_IMAGE_PNG_SUPPORT
+
+#include "../sd_mmc_card/sd_mmc_card.h"
+#include "esphome/components/image/image.h"
+#include "esphome/core/component.h"
+#include "esphome/core/defines.h"
+#include "esphome/core/helpers.h"
+
+#include "image_decoder.h"
+
+namespace esphome
+{
+    namespace local_image
+    {
+
+        using t_http_codes = enum {
+            HTTP_CODE_OK = 200,
+            HTTP_CODE_NOT_MODIFIED = 304,
+            HTTP_CODE_NOT_FOUND = 404,
+        };
+
+        /**
+         * @brief Format that the image is encoded with.
+         */
+        enum ImageFormat
+        {
+            /** Automatically detect from MIME type. Not supported yet. */
+            AUTO,
+            /** JPEG format. */
+            JPEG,
+            /** PNG format. */
+            PNG,
+            /** BMP format. */
+            BMP,
+        };
+
+        /**
+         * @brief Download an image from a given URL, and decode it using the specified decoder.
+         * The image will then be stored in a buffer, so that it can be re-displayed without the
+         * need to re-download or re-decode.
+         */
+        // class Image : public PollingComponent,
+        //                     public image::Image,
+        //                     public Parented<esphome::http_request::HttpRequestComponent> {
+
+        class LocalImage : public PollingComponent,
+                           public image::Image
+        {
+        public:
+            /**
+             * @brief Construct a new LocalImage object.
+             *
+             * @param path
+             * @param width Desired width of the target image area.
+             * @param height Desired height of the target image area.
+             * @param format Format that the image is encoded in (@see ImageFormat).
+             * @param buffer_size Size of the buffer used to download the image.
+             */
+            LocalImage(sd_mmc_card::SdMmc *card, const std::string &path, int width, int height, ImageFormat format, image::ImageType type,
+                       image::Transparency transparency, uint32_t buffer_size);
+
+            void draw(int x, int y, display::Display *display, Color color_on, Color color_off) override;
+
+            void update() override;
+            void loop() override;
+            void map_chroma_key(Color &color);
+
+            /** Set the URL to download the image from. */
+            void set_path(const std::string &path)
+            {
+                if (this->validate_path_(path))
+                {
+                    this->path_ = path;
+                }
+            }
+
+            void set_sd_mmc_card(sd_mmc_card::SdMmc *);
+            
+            /**
+             * @brief Set the image that needs to be shown as long as the downloaded image
+             *  is not available.
+             *
+             * @param placeholder Pointer to the (@link Image) to show as placeholder.
+             */
+            void set_placeholder(image::Image *placeholder) { this->placeholder_ = placeholder; }
+
+            /**
+             * Release the buffer storing the image. The image will need to be downloaded again
+             * to be able to be displayed.
+             */
+            void release();
+
+            /**
+             * Resize the download buffer
+             *
+             * @param size The new size for the download buffer.
+             */
+            size_t resize_download_buffer(size_t size) { return this->download_buffer_.resize(size); }
+
+            void add_on_finished_callback(std::function<void()> &&callback);
+            void add_on_error_callback(std::function<void()> &&callback);
+
+        protected:
+            bool validate_path_(const std::string &path);
+
+            RAMAllocator<uint8_t> allocator_{};
+
+            uint32_t get_buffer_size_() const { return get_buffer_size_(this->buffer_width_, this->buffer_height_); }
+            int get_buffer_size_(int width, int height) const { return (this->get_bpp() * width + 7u) / 8u * height; }
+
+            int get_position_(int x, int y) const { return (x + y * this->buffer_width_) * this->get_bpp() / 8; }
+
+            ESPHOME_ALWAYS_INLINE bool is_auto_resize_() const { return this->fixed_width_ == 0 || this->fixed_height_ == 0; }
+
+            /**
+             * @brief Resize the image buffer to the requested dimensions.
+             *
+             * The buffer will be allocated if not existing.
+             * If the dimensions have been fixed in the yaml config, the buffer will be created
+             * with those dimensions and not resized, even on request.
+             * Otherwise, the old buffer will be deallocated and a new buffer with the requested
+             * allocated
+             *
+             * @param width
+             * @param height
+             * @return 0 if no memory could be allocated, the size of the new buffer otherwise.
+             */
+            size_t resize_(int width, int height);
+
+            /**
+             * @brief Draw a pixel into the buffer.
+             *
+             * This is used by the decoder to fill the buffer that will later be displayed
+             * by the `draw` method. This will internally convert the supplied 32 bit RGBA
+             * color into the requested image storage format.
+             *
+             * @param x Horizontal pixel position.
+             * @param y Vertical pixel position.
+             * @param color 32 bit color to put into the pixel.
+             */
+            void draw_pixel_(int x, int y, Color color);
+
+            // void end_connection_();
+
+            CallbackManager<void()> load_finished_callback_{};
+            CallbackManager<void()> download_error_callback_{};
+
+            // std::shared_ptr<http_request::HttpContainer> downloader_{nullptr};
+
+            sd_mmc_card::SdMmc *sd_mmc_card_{nullptr};
+
+            std::unique_ptr<ImageDecoder> decoder_{nullptr};
+
+            uint8_t *source_buffer_;
+            size_t source_size_ = 0;
+            uint8_t *buffer_;
+            bool loded = false;
+            DownloadBuffer download_buffer_;
+            /**
+             * This is the *initial* size of the download buffer, not the current size.
+             * The download buffer can be resized at runtime; the download_buffer_initial_size_
+             * will *not* change even if the download buffer has been resized.
+             */
+            size_t download_buffer_initial_size_;
+
+            const ImageFormat format_;
+            image::Image *placeholder_{nullptr};
+
+            std::string path_{""};
+
+            /** width requested on configuration, or 0 if non specified. */
+            const int fixed_width_;
+            /** height requested on configuration, or 0 if non specified. */
+            const int fixed_height_;
+            /**
+             * Actual width of the current image. If fixed_width_ is specified,
+             * this will be equal to it; otherwise it will be set once the decoding
+             * starts and the original size is known.
+             * This needs to be separate from "BaseImage::get_width()" because the latter
+             * must return 0 until the image has been decoded (to avoid showing partially
+             * decoded images).
+             */
+            int buffer_width_;
+            /**
+             * Actual height of the current image. If fixed_height_ is specified,
+             * this will be equal to it; otherwise it will be set once the decoding
+             * starts and the original size is known.
+             * This needs to be separate from "BaseImage::get_height()" because the latter
+             * must return 0 until the image has been decoded (to avoid showing partially
+             * decoded images).
+             */
+            int buffer_height_;
+
+            // time_t start_time_;
+
+            friend bool ImageDecoder::set_size(int width, int height);
+            friend void ImageDecoder::draw(int x, int y, int w, int h, const Color &color);
+        };
+
+        template <typename... Ts>
+        class LocalImageSetPathAction : public Action<Ts...>
+        {
+        public:
+            LocalImageSetPathAction(LocalImage *parent) : parent_(parent) {}
+            TEMPLATABLE_VALUE(std::string, path)
+            void play(Ts... x) override
+            {
+                this->parent_->set_path(this->path_.value(x...));
+                this->parent_->update();
+            }
+
+        protected:
+            LocalImage *parent_;
+        };
+
+        template <typename... Ts>
+        class LocalImageReleaseAction : public Action<Ts...>
+        {
+        public:
+            LocalImageReleaseAction(LocalImage *parent) : parent_(parent) {}
+            void play(Ts... x) override { this->parent_->release(); }
+
+        protected:
+            LocalImage *parent_;
+        };
+
+
+        template <typename... Ts>
+        class LocalImageReloadAction : public Action<Ts...>
+        {
+        public:
+            LocalImageReloadAction(LocalImage *parent) : parent_(parent) {}
+            TEMPLATABLE_VALUE(std::string, path)
+            void play(Ts... x) override
+            {
+                this->parent_->set_path(this->path_.value(x...));
+                this->parent_->update();
+            }
+
+        protected:
+            LocalImage *parent_;
+        };
+
+
+
+        class LoadFinishedTrigger : public Trigger<>
+        {
+        public:
+            explicit LoadFinishedTrigger(LocalImage *parent)
+            {
+                parent->add_on_finished_callback([this]()
+                                                 { this->trigger(); });
+            }
+        };
+        
+        class LoadErrorTrigger : public Trigger<>
+        {
+        public:
+            explicit LoadErrorTrigger(LocalImage *parent)
+            {
+                parent->add_on_error_callback([this]()
+                                              { this->trigger(); });
+            }
+        };
+
+    } // namespace online_image
+} // namespace esphome
